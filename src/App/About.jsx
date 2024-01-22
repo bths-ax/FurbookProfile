0b29efe9dadbc5a31f716b3b1bbfaf409dd0ce74
bths-ax <ardenx@nycstudents.net>
2024-01-22 09:12:53 -0500

    prepare for refactoring

---
+++ src/App/About.jsx
@@ -0,0 +1,43 @@
+import takotaProfile from './assets/takota.png'
+
+export default function About() {
+	return (
+		<div className='container'>
+			<div className='row'>
+				<div className='col-4 picture-box'>
+					<img src={takotaProfile} className='pic' />
+					<h3>Takota McConner</h3>
+					<p className='list-group-item'>
+						&quot;Laziness is nothing more than the habit of resting
+						before you get tired.&quot; -Jules Renard
+					</p>
+				</div>
+
+				<div className='col-7 offset-1 about-box'>
+					<ul className='list-group'>
+						<p className='list-group-item heading'>About</p>
+						<p className='list-group-item'>Age: 12</p>
+						<p className='list-group-item'>Occupation: Pet</p>
+						<p className='list-group-item'>
+							Education: Wild Cat Academy
+						</p>
+						<p className='list-group-item'>
+							Favorite Bands: Cat Stevens, Def Leopard, Atomic
+							Kitten, Lionel Ritchie
+						</p>
+						<p className='list-group-item'>
+							Contact Info: Meow at me at the fence behind Burger
+							King on 242nd and Broadway
+						</p>
+						<p className='list-group-item'>
+							Relationship Status: Single
+						</p>
+						<p className='list-group-item'>
+							Places Lived: Paris, France and Bronx, New York
+						</p>
+					</ul>
+				</div>
+			</div>
+		</div>
+	)
+}
---
+++ src/App/Posts.jsx
@@ -0,0 +1,92 @@
+export default function Posts() {
+	return (
+		<div className='container'>
+			<div className='row'>
+				<div className='col-12'>
+					<div className='list-group'>
+						<a
+							href='#'
+							className='list-group-item list-group-item-action active'
+						>
+							<div>
+								<h5>Recent Posts</h5>
+							</div>
+						</a>
+						<a
+							href='#'
+							className='list-group-item list-group-item-action'
+						>
+							<div>
+								<h5>Takota McConner Says:</h5>
+								<small className='text-muted'>3 days ago</small>
+							</div>
+							<p>
+								Did nothing today, just layed around and ate
+								food #bestdayever
+							</p>
+							<input placeholder='comment..' />
+						</a>
+						<a
+							href='#'
+							className='list-group-item list-group-item-action'
+						>
+							<div>
+								<h5>Takota McConner Says:</h5>
+								<small className='text-muted'>4 days ago</small>
+							</div>
+							<p>
+								Didn&apos;t do much today, just layed around and
+								ate food #yolo
+							</p>
+							<input placeholder='comment..' />
+						</a>
+						<a
+							href='#'
+							className='list-group-item list-group-item-action'
+						>
+							<div>
+								<h5>Takota McConner Says:</h5>
+								<small className='text-muted'>7 days ago</small>
+							</div>
+							<p>
+								Nothing accomplished today, just layed around
+								and ate food #livingmybestlife
+							</p>
+							<input placeholder='comment..' />
+						</a>
+						<a
+							href='#'
+							className='list-group-item list-group-item-action'
+						>
+							<div>
+								<h5>Takota McConner Says:</h5>
+								<small className='text-muted'>8 days ago</small>
+							</div>
+							<p>
+								Slept all day and ate a few meals
+								#killingthegame
+							</p>
+							<input placeholder='comment..' />
+						</a>
+						<a
+							href='#'
+							className='list-group-item list-group-item-action'
+						>
+							<div>
+								<h5>Takota McConner Says:</h5>
+								<small className='text-muted'>
+									12 days ago
+								</small>
+							</div>
+							<p>
+								Was taken to the vet - considering puking in my
+								owners shoes. Opinions?
+							</p>
+							<input type='text' placeholder=' comment..' />
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
